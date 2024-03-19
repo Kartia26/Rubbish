70baa2d4606b0a4ebd5b1609a54a9c91a664cd7f
Kartia26 <163066350+Kartia26@users.noreply.github.com>
2024-03-20 10:28:45 +1300

    first upload

---
+++ MY REAL ASSESSMENT QUIZ THINGY.py
@@ -0,0 +1,53 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:\Users\22329\CSCI ASSESMENT QUIZ.py ================
+hello
+Traceback (most recent call last):
+  File "C:\Users\22329\CSCI ASSESMENT QUIZ.py", line 15, in <module>
+    for x in quiz:
+NameError: name 'quiz' is not defined. Did you mean: 'quiz1'?
+
+================ RESTART: C:\Users\22329\CSCI ASSESMENT QUIZ.py ================
+hello
+What's Taika Waititi's Most Recent Film? a. Jojo Rabbits  b. Thor Love And Thunder c. Next Goal Wins d. Thor Ragnarok
+Enter Your AnswerC
+What Part Of NZ Does Taika Waititi Come From? a. auckland b. Wellington c. Invercargill d. Taupō
+Enter Your AnswerB
+If Taika Waittit Was Born In 1975 What Age Would He Be This Year? a. 48  b. 45 c. 50 d. 49.
+Enter Your AnswerD
+Which is Taika Waitit’s Current Spouse? a. Chelsea Winstanley b. Rita Ora c. Loren Horsley
+Enter Your AnswerB
+Which Film Did Taika Waititi Not Direct? a. Avengers Endgame b. Jojo Rabbit c. What We Do In The Shadows  d. Boy
+Enter Your AnswerA
+Which Film Of Taika Waititi’s Had A Main Character With The Name Of Ricky Baker?  a. Hunt For Widerpeople b. Hunt For Wildpeople  c. Hunt For The Wilderpeople d. Hunt For The Crazypeople
+Enter Your AnswerC
+How Many Children Does Taika Waititi Have? a. 2 b. 3 c. 1 d. 4
+Enter Your AnswerA
+Traceback (most recent call last):
+  File "C:\Users\22329\CSCI ASSESMENT QUIZ.py", line 18, in <module>
+    while answer not in options:
+NameError: name 'options' is not defined
+
+================ RESTART: C:\Users\22329\CSCI ASSESMENT QUIZ.py ================
+hello
+What's Taika Waititi's Most Recent Film? a. Jojo Rabbits  b. Thor Love And Thunder c. Next Goal Wins d. Thor Ragnarok
+Enter Your Answera
+What Part Of NZ Does Taika Waititi Come From? a. auckland b. Wellington c. Invercargill d. Taupō
+Enter Your Answerc
+If Taika Waittit Was Born In 1975 What Age Would He Be This Year? a. 48  b. 45 c. 50 d. 49.
+Enter Your Answerb
+Which is Taika Waitit’s Current Spouse? a. Chelsea Winstanley b. Rita Ora c. Loren Horsley
+Enter Your Answer
+Which Film Did Taika Waititi Not Direct? a. Avengers Endgame b. Jojo Rabbit c. What We Do In The Shadows  d. Boy
+Enter Your Answera
+Which Film Of Taika Waititi’s Had A Main Character With The Name Of Ricky Baker?  a. Hunt For Widerpeople b. Hunt For Wildpeople  c. Hunt For The Wilderpeople d. Hunt For The Crazypeople
+Enter Your Answerd
+How Many Children Does Taika Waititi Have? a. 2 b. 3 c. 1 d. 4
+Enter Your Answer
+Enter Your Answer
+incorrect
+Enter Your Answera
+correct, your score is 5
+you lost
+
